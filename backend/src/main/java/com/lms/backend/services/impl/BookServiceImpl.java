3893b0ee55bf841d3fc390838a53367f938a8a87
manas <barikmanas78@gmail.com>
2023-10-26 19:54:39 +0530

    book service add

---
+++ backend/src/main/java/com/lms/backend/services/impl/BookServiceImpl.java
@@ -0,0 +1,91 @@
+package com.lms.backend.services.impl;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.lms.backend.constants.BookStatus;
+import com.lms.backend.model.Book;
+import com.lms.backend.repository.BookRepository;
+import com.lms.backend.services.BookService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    @Override
+    public Book createBook(Book book) {
+        var bookId = UUID.randomUUID().toString();
+        var currentDate = LocalDateTime.now();
+        var status = BookStatus.OUTOFSTOCK;
+
+        book.setBookId(bookId);
+        book.setCreatedDate(currentDate);
+        book.setUpdatedDate(currentDate);
+        book.setStatus(status);
+
+        bookRepository.save(book);
+        return book;
+    }
+
+    @Override
+    public Book updateBook(Book book, String bookId) {
+        var oldBook = bookRepository.findById(bookId).orElseThrow();
+
+        oldBook.setBookName(book.getBookName());
+        oldBook.setAuthor(book.getAuthor());
+        oldBook.setDescription(book.getDescription());
+        oldBook.setReferenceNumber(book.getReferenceNumber());
+        oldBook.setUpdatedDate(LocalDateTime.now());
+
+        bookRepository.save(oldBook);
+        return oldBook;
+    }
+
+    @Override
+    public Book deleteBook(Book book, String bookId) {
+        var oldBook = bookRepository.findById(bookId).orElseThrow();
+
+        oldBook.setStatus(BookStatus.DEACTIVE);
+        oldBook.setUpdatedDate(LocalDateTime.now());
+
+        bookRepository.save(oldBook);
+        return oldBook;
+    }
+
+    @Override
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public List<Book> getAllActiveBooks() {
+        return bookRepository.findAllByStatus(BookStatus.ACTIVE);
+    }
+
+    @Override
+    public List<Book> getAllDeactiveBooks() {
+        return bookRepository.findAllByStatus(BookStatus.DEACTIVE);
+    }
+
+    @Override
+    public Book updateStock(String bookId, int stock) {
+        if (stock == 0) {
+            throw new RuntimeException("stock can't be 0");
+        }
+
+        var book = bookRepository.findById(bookId).orElseThrow();
+        book.setStatus(BookStatus.ACTIVE);
+        book.setAvailableStock(stock);
+
+        bookRepository.save(book);
+        return book;
+    }
+
+}
