6a0554f07de95cfac625947871266b474079c635
manas <barikmanas78@gmail.com>
2023-10-26 19:54:54 +0530

    book repository update

--- backend/src/main/java/com/lms/backend/repository/BookRepository.java
+++ backend/src/main/java/com/lms/backend/repository/BookRepository.java
@@ -1,9 +1,12 @@
 package com.lms.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import com.lms.backend.constants.BookStatus;
 import com.lms.backend.model.Book;
 
-public interface BookRepository extends MongoRepository<Book, String>{
-    
+public interface BookRepository extends MongoRepository<Book, String> {
+    List<Book> findAllByStatus(BookStatus status);
 }
