5e61fd16499b9b5a874eced887784353b05f3ba2
smruti-banty <srnayak165@gmail.com>
2023-10-27 19:36:10 +0530

    shdcn config add

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Button>Click me</Button>
+      <button className="text-red-800">click me</button>
+    </>
+  );
+}
+
+export default App;
