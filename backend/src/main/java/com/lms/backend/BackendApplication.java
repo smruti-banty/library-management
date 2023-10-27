9939e10580f2812ecebc1be5a8f17a3fefac07d8
smruti-banty <srnayak165@gmail.com>
2023-10-27 17:03:30 +0530

    support cors requests

--- backend/src/main/java/com/lms/backend/BackendApplication.java
+++ backend/src/main/java/com/lms/backend/BackendApplication.java
@@ -2,11 +2,13 @@ package com.lms.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@CrossOrigin
 @OpenAPIDefinition(info = @Info(title = "Library Management", version = "v1", description = "Manage library system"))
 public class BackendApplication {
 
