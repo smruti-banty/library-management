04d7771dc34b06f544d1309230e893f429a8a149
smruti-banty <srnayak165@gmail.com>
2023-10-27 16:45:31 +0530

    Api defination add for swagger

--- backend/src/main/java/com/lms/backend/BackendApplication.java
+++ backend/src/main/java/com/lms/backend/BackendApplication.java
@@ -3,6 +3,10 @@ package com.lms.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Library Management", version = "v1", description = "Manage library system"))
 @SpringBootApplication
 public class BackendApplication {
 
