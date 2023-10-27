9e21981870bb5338a66bf2f67ba18155de5c3bff
gudulu <rrnayak165@gmail.com>
2023-10-27 12:21:10 +0530

    UserService add.

--- backend/src/main/java/com/lms/backend/repository/UserRepository.java
+++ backend/src/main/java/com/lms/backend/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package com.lms.backend.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import com.lms.backend.constants.UserRole;
 import com.lms.backend.model.User;
 
 public interface UserRepository extends MongoRepository<User, String> {
+    List<User> findAllByUserRole(UserRole userRole);
 
+    Optional<User> findByReferenceNumber(String referenceNumber);
 }
