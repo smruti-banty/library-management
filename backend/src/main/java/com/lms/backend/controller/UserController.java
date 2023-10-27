d65153a514e34fda73463d0bc9ac8609a1a912f5
gudulu <rrnayak165@gmail.com>
2023-10-27 13:39:13 +0530

    User controller add

---
+++ backend/src/main/java/com/lms/backend/controller/UserController.java
@@ -0,0 +1,82 @@
+package com.lms.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import com.lms.backend.dto.UserRequestDto;
+import com.lms.backend.model.User;
+import com.lms.backend.services.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@RequestBody UserRequestDto userRequestDto) {
+        var user = new User();
+        BeanUtils.copyProperties(userRequestDto, user);
+        return userService.createUser(user);
+    }
+
+    @PostMapping("/createAdmin")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createAdmin(@RequestBody UserRequestDto userRequestDto) {
+        var user = new User();
+        BeanUtils.copyProperties(userRequestDto, user);
+        return userService.createAdmin(user);
+    }
+
+    @PutMapping("/update/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public User updateUser(@RequestBody UserRequestDto userRequestDto, @PathVariable String userId) {
+        var user = new User();
+        BeanUtils.copyProperties(userRequestDto, user);
+        return userService.updateUser(user, userId);
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public User deleteUser(@PathVariable String userId) {
+        return userService.deleteUser(userId);
+    }
+
+    @GetMapping("/users")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/admins")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getAdmins() {
+        return userService.getAdmins();
+    }
+
+    @GetMapping("/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public User getUserById(@PathVariable String userId) {
+        return userService.getUserById(userId);
+    }
+
+    @GetMapping("/byReferenceNumber/{referenceNumber}")
+    @ResponseStatus(HttpStatus.OK)
+    public User getUserByReferenceNumber(@PathVariable String referenceNumber) {
+        return userService.getUserByReferenceNumber(referenceNumber);
+    }
+
+    @PutMapping("/approve/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public User approveUser(@PathVariable String userId) {
+        return userService.approveUser(userId);
+    }
+}
