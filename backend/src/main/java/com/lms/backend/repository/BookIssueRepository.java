52e355f84fd5bff67175742936e24941f9ce98bf
manas <barikmanas78@gmail.com>
2023-10-28 18:05:09 +0530

    repo created

--- backend/src/main/java/com/lms/backend/repository/BookIssueRepository.java
+++ backend/src/main/java/com/lms/backend/repository/BookIssueRepository.java
@@ -2,6 +2,8 @@ package com.lms.backend.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface BookIssueRepository extends MongoRepository<BookIssueRepository, String> {
+import com.lms.backend.model.BookIssue;
+
+public interface BookIssueRepository extends MongoRepository<BookIssue, String> {
 
 }
