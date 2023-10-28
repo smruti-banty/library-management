706c3206d6c94d039704f7be50a3b0f6a93b8143
smruti-banty <srnayak165@gmail.com>
2023-10-28 18:14:34 +0530

    cors support modify

--- backend/src/main/java/com/lms/backend/config/CorsConfig.java
+++ backend/src/main/java/com/lms/backend/config/CorsConfig.java
@@ -11,6 +11,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+        .allowedOrigins("*") 
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+        .allowedHeaders("*");
     }
 }
