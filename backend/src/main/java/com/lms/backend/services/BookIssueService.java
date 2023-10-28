7b2e68cc1fb2d10379e0998792c5f3acd64b837c
manas <barikmanas78@gmail.com>
2023-10-28 17:29:55 +0530

    method define in book issue service

--- backend/src/main/java/com/lms/backend/services/BookIssueService.java
+++ backend/src/main/java/com/lms/backend/services/BookIssueService.java
@@ -3,5 +3,7 @@ package com.lms.backend.services;
 import com.lms.backend.model.BookIssue;
 
 public interface BookIssueService {
-    BookIssue issueBook(BookIssue bookIssue);
+    BookIssue issueBook(BookIssue bookIssue);    
+    BookIssue reurnBook(BookIssue bookIssue);
+
 }
