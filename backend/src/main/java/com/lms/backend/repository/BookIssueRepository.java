bc0bd279c6804bcae416e4cb1500e675340cc1e5
manas <barikmanas78@gmail.com>
2023-10-28 16:20:36 +0530

    generic type add

--- backend/src/main/java/com/lms/backend/repository/BookIssueRepository.java
+++ backend/src/main/java/com/lms/backend/repository/BookIssueRepository.java
@@ -2,6 +2,6 @@ package com.lms.backend.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface BookIssueRepository extends MongoRepository {
+public interface BookIssueRepository extends MongoRepository<BookIssueRepository, String> {
 
 }
