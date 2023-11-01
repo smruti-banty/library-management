afe9df6885b0697c386ed510bea188660607d668
smruti-banty <srnayak165@gmail.com>
2023-11-02 00:02:12 +0530

    user response dto modify

--- backend/src/main/java/com/lms/backend/dto/UserResponseDto.java
+++ backend/src/main/java/com/lms/backend/dto/UserResponseDto.java
@@ -1,14 +1,8 @@
 package com.lms.backend.dto;
 
-import java.time.LocalDateTime;
-
 public record UserResponseDto(
-                String userId, String firstName,
-                String lastName, String email,
-                String referenceNumber,
-                String profilePic,
-                LocalDateTime updatedDate,
-                LocalDateTime createdDate,
-                String createdBy,
-                String updatedBy) {
+        String firstName,
+        String lastName, String email,
+        String referenceNumber,
+        String profilePic) {
 }
