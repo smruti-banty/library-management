b6832009272220723ba9f24df1531df262b1b292
manas <barikmanas78@gmail.com>
2023-11-03 22:00:53 +0530

    naming method for retrive last five

--- backend/src/main/java/com/lms/backend/repository/TransactionRepository.java
+++ backend/src/main/java/com/lms/backend/repository/TransactionRepository.java
@@ -1,9 +1,14 @@
 package com.lms.backend.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.lms.backend.model.Transaction;
 
 public interface TransactionRepository extends MongoRepository<Transaction, String> {
     Transaction findByBookIssueId(String bookIssueId);
+
+    List<Transaction> findTop5ByCreatedDateBeforeOrderByCreatedDateDesc(LocalDateTime currentDateTime);
 }
