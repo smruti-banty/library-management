4e2a60a3bd8c8b8e6e06ad7bfe0cd8941562cd71
smruti-banty <srnayak165@gmail.com>
2023-11-03 23:20:15 +0530

    avtar, project title, icon add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import SideBar from "./components/ui/sidebar";
 import { Outlet, useLocation } from "react-router-dom";
 import { getAdminMenus } from "./common/menus";
 import { Toaster } from "./components/ui/toaster";
-
 function App() {
   const pathName = useLocation().pathname;
   const menus = getAdminMenus();
@@ -13,12 +12,14 @@ function App() {
       <SideBar menus={menus} active={pathName}></SideBar>
       <main className="flex-1">
         <header className="bg-gray-50 dark:bg-gray-800 p-5 shadow-md">
-          <h1>Heading</h1>
+          <h1 className="text-right text-xl">
+            👤
+          </h1>
         </header>
         <section className="p-10">
           <Outlet />
         </section>
-        <Toaster/>
+        <Toaster />
       </main>
     </div>
   );
