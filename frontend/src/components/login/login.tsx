00320ee872dd5127d11911b83edbfd21ae5c9788
smruti-banty <srnayak165@gmail.com>
2023-11-06 19:31:33 +0530

    add toaster

--- frontend/src/components/login/login.tsx
+++ frontend/src/components/login/login.tsx
@@ -2,6 +2,7 @@ import "./login.css";
 import { useState } from "react";
 import Signup from "./signup";
 import Signin from "./signin";
+import { Toaster } from "../ui/toaster";
 
 const Login = () => {
   const [isSignUp, setIsSignUp] = useState(true);
@@ -22,6 +23,7 @@ const Login = () => {
         <Signup handleSignInClick={handleSignInClick} />
         <Signin handleSignUpClick={handleSignUpClick} />
       </div>
+      <Toaster />
     </main>
   );
 };
